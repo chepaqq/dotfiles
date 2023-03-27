e28aa04425fb51cc404c8750165e742e58c18a6f
chepaqq <chepaqq@protonmail.com>
2023-03-27 15:03:35 +0300

    Add netrw plugin

--- neovim/.config/nvim/lua/core/options.lua
+++ neovim/.config/nvim/lua/core/options.lua
@@ -64,3 +64,10 @@ opt.wildignore:append("**/.git/*")
 
 -- Disable nvim intro
 opt.shortmess:append("sI")
+
+-- Better Netrw
+g.netrw_banner = 0 -- Hide banner
+g.netrw_altv = 1 -- Open with right splitting
+g.netrw_liststyle = 3 -- Tree-style view
+g.netrw_browse_split = 3 -- Open file in new tab
+g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignore
