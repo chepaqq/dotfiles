e17b6914bcfc521c0a1d76b145c67324f3fddaab
chepaqq <chepaqq@protonmail.com>
2023-03-30 17:42:38 +0300

    Refactor nvim-cmp config

--- neovim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -45,16 +45,18 @@ return {
     },
   },
   config = function()
-    local cmp = require("cmp")
     local has_words_before = function()
+      unpack = unpack or table.unpack
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
     cmp.setup({
       -- Load snippet support
       snippet = {
         expand = function(args)
-          require("luasnip").lsp_expand(args.body)
+          luasnip.lsp_expand(args.body)
         end,
       },
 
@@ -82,8 +84,8 @@ return {
         ["<C-n>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif require("luasnip").expand_or_jumpable() then
-            require("luasnip").expand_or_jump()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
           elseif has_words_before() then
             cmp.complete()
           else
@@ -97,8 +99,8 @@ return {
         ["<C-p>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif require("luasnip").jumpable(-1) then
-            require("luasnip").jump(-1)
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
           else
             fallback()
           end
@@ -115,26 +117,27 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         }),
-
-        -- Tab mapping
-        ["<Tab>"] = function(fallback)
+        ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif require("luasnip").expand_or_jumpable() then
-            require("luasnip").expand_or_jump()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
           else
             fallback()
           end
-        end,
-        ["<S-Tab>"] = function(fallback)
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif require("luasnip").jumpable(-1) then
-            require("luasnip").jump(-1)
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
           else
             fallback()
           end
-        end,
+        end, { "i", "s" }),
       },
       -- Load sources, see: https://github.com/topics/nvim-cmp
       sources = {
@@ -156,7 +159,7 @@ return {
       [[
       autocmd FileType sql setlocal omnifunc=vim_dadbod_completion#omni
       autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
-    ]],
+      ]],
       false
     )
     require("cmp").setup.cmdline(":", {
