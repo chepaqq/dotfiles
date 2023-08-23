4c87b595618bafe8fb2c4c79019c15724b2398f1
chepaqq <chepaqq@protonmail.com>
2023-08-23 13:07:32 +0300

    Load all keys for comment.nvim

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -13,7 +13,7 @@ return {
   },
   {
     "numToStr/Comment.nvim",
-    keys = { "gcc", "gbc", { "gc", mode = "v" }, { "gb", mode = "v" } },
+    event = "BufReadPre",
     config = function()
       require("Comment").setup({
         ignore = "^$",
