c084e68ee876cfe443db94530fbe9a51455873c2
chepaqq <chepaqq@protonmail.com>
2023-08-23 12:51:36 +0300

    Add new sources for null-ls

--- neovim/.config/nvim/lua/plugins/null-ls.lua
+++ neovim/.config/nvim/lua/plugins/null-ls.lua
@@ -4,25 +4,26 @@ return {
   config = function()
     local null_ls = require("null-ls")
     local sources = {
+      null_ls.builtins.code_actions.gitsigns,
+      null_ls.builtins.code_actions.gomodifytags,
+      null_ls.builtins.code_actions.impl,
+      null_ls.builtins.code_actions.refactoring,
+      null_ls.builtins.code_actions.shellcheck,
       null_ls.builtins.diagnostics.golangci_lint,
-      null_ls.builtins.diagnostics.staticcheck,
       null_ls.builtins.diagnostics.revive,
       null_ls.builtins.formatting.beautysh,
       null_ls.builtins.formatting.black,
       null_ls.builtins.formatting.gofumpt,
+      null_ls.builtins.formatting.goimports,
       null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.sql_formatter,
-      null_ls.builtins.code_actions.gitsigns,
-      null_ls.builtins.code_actions.gomodifytags,
-      null_ls.builtins.code_actions.refactoring,
-      null_ls.builtins.code_actions.shellcheck,
       null_ls.builtins.hover.printenv,
       null_ls.builtins.formatting.clang_format.with({
         extra_args = { "--style={BasedOnStyle: llvm, IndentWidth: 4, UseTab: Never}" },
       }),
       null_ls.builtins.formatting.golines.with({
         extra_args = {
-          "--max-len=180",
+          "--max-len=100",
           "--base-formatter=gofumpt",
         },
       }),
