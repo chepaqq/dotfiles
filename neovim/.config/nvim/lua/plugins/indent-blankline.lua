df290ae471ac8f4a7032d1b4f09ae921eedb3267
chepaqq <chepaqq@protonmail.com>
2023-10-04 12:11:30 +0300

    Update indent-blankline plugin

--- neovim/.config/nvim/lua/plugins/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,30 +1,32 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   event = { "BufReadPost", "BufNewFile" },
   config = function()
-    local indent_blankline = require("indent_blankline")
+    local indent_blankline = require("ibl")
     indent_blankline.setup({
-      char = "▏",
-      use_treesitter = true,
-      show_first_indent_level = false,
-      filetype_exclude = {
-        "lspinfo",
-        "packer",
-        "checkhealth",
-        "help",
-        "man",
-        "dashboard",
-        "git",
-        "markdown",
-        "text",
-        "terminal",
-        "NvimTree",
-      },
-      buftype_exclude = {
-        "terminal",
-        "nofile",
-        "quickfix",
-        "prompt",
+      indent = { char = "▏" },
+      scope = { enabled = true },
+      exclude = {
+        filetypes = {
+          "lspinfo",
+          "packer",
+          "checkhealth",
+          "help",
+          "man",
+          "dashboard",
+          "git",
+          "markdown",
+          "text",
+          "terminal",
+          "NvimTree",
+        },
+        buftypes = {
+          "terminal",
+          "nofile",
+          "quickfix",
+          "prompt",
+        },
       },
     })
   end,
